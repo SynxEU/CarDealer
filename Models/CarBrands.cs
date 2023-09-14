6a0e7f9fdb9e924c862b7af4b39eddde4e96da89
SynxEU <126554945+SynxEU@users.noreply.github.com>
2023-09-14 08:48:35 +0200

    Add files via upload

---
+++ Models/CarBrands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerProjekt.Models
+{
+    public enum Carbrand
+    {
+        McLaren = 0,
+        Koenigsegg = 1,
+        Aston_Martin = 2,
+        Bugatti = 3,
+        Bentley = 4,
+        Maserati = 5,
+        Zenvo = 6,
+        Lamborghini = 7
+    }
+}
