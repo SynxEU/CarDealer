796bb7bad9b8d420c04005b41ea67b91b6237a45
SynxEU <126554945+SynxEU@users.noreply.github.com>
2023-09-14 08:49:34 +0200

    Update Cardealer.cs

--- Cardealer.cs
+++ Cardealer.cs
@@ -35,7 +35,7 @@ namespace CarDealerProjekt
         public string SQLConnection()
         {
             IConfiguration config = new ConfigurationBuilder()
-                .SetBasePath("C:\\Users\\jonkn0001.PCSYD\\Downloads\\CarDealerProjekt\\CarDealerProjekt")
+                .SetBasePath("Path to appsettings")
                 .AddJsonFile("appsettings.json")
                 .Build();
             string connectionString = config.GetConnectionString("Default");
